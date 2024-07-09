07c284195fe203820bf53dddb7856cca4287c071
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2024-07-09 17:16:44 +0300

    refactor MainActivity

--- app/src/main/java/com/example/financewallet/MainActivity.kt
+++ app/src/main/java/com/example/financewallet/MainActivity.kt
@@ -1,27 +1,17 @@
 package com.example.financewallet
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
-import androidx.core.view.WindowInsetsCompat
+import com.example.financewallet.presentation.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-
         if (savedInstanceState == null) {
             loadFragment(HomeFragment())
-        }
-
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
         }
     }
 
