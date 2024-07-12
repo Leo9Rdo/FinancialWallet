4b94d7e47775575fb7e42f9d61326814ce689a2d
John <muravei20004@mail.ru>
2024-07-12 17:07:01 +0300

    delete commet's + use ktlint

--- app/src/main/java/com/example/financewallet/MainActivity.kt
+++ app/src/main/java/com/example/financewallet/MainActivity.kt
@@ -8,10 +8,7 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.financewallet.databinding.ActivityMainBinding
-import com.example.financewallet.presentation.assetList.AssetListFragment
 import com.example.financewallet.presentation.home.HomeFragment
-import com.example.financewallet.presentation.portfolio.PortfolioListFragment
-import com.example.financewallet.presentation.settings.SettingsFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -24,43 +21,20 @@ class MainActivity : AppCompatActivity() {
         setContentView(_binding.root)
         replaceFragment(HomeFragment())
 
-        val navView:BottomNavigationView=_binding.navigationViewBtm
+        val navView: BottomNavigationView = _binding.navigationViewBtm
 
-        val navController= findNavController(R.id.fragment_container_view)
+        val navController = findNavController(R.id.fragment_container_view)
         val appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.navigation_home,R.id.navigation_assets, R.id.navigation_portfolio,R.id.navigation_settings
+                R.id.navigation_home,
+                R.id.navigation_assets,
+                R.id.navigation_portfolio,
+                R.id.navigation_settings
             )
         )
-        setupActionBarWithNavController(navController,appBarConfiguration)
+        setupActionBarWithNavController(navController, appBarConfiguration)
 
         navView.setupWithNavController(navController)
-
-    //        _binding.navigationViewBtm.setOnNavigationItemSelectedListener { item ->
-    //            when (item.itemId) {
-    //                R.id.navigation_home -> {
-    //                    replaceFragment(HomeFragment())
-    //                    true
-    //                }
-    //
-    //                R.id.navigation_portfolio -> {
-    //                    replaceFragment(PortfolioListFragment())
-    //                    true
-    //                }
-    //
-    //                R.id.navigation_assets -> {
-    //                    replaceFragment(AssetListFragment())
-    //                    true
-    //                }
-    //
-    //                R.id.navigation_settings -> {
-    //                    replaceFragment(SettingsFragment())
-    //                    true
-    //                }
-    //
-    //                else -> false
-    //            }
-    //        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
