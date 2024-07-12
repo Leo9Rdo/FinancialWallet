9a1beb4c43da60033fb9406da65d9cd775cc7ee6
John <muravei20004@mail.ru>
2024-07-12 17:01:37 +0300

    Add Navigate Component + corrected xml's

--- app/src/main/java/com/example/financewallet/MainActivity.kt
+++ app/src/main/java/com/example/financewallet/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.financewallet
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.financewallet.databinding.ActivityMainBinding
 import com.example.financewallet.presentation.assetList.AssetListFragment
 import com.example.financewallet.presentation.home.HomeFragment
 import com.example.financewallet.presentation.portfolio.PortfolioListFragment
 import com.example.financewallet.presentation.settings.SettingsFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
@@ -19,31 +24,43 @@ class MainActivity : AppCompatActivity() {
         setContentView(_binding.root)
         replaceFragment(HomeFragment())
 
-        _binding.navigationViewBtm.setOnNavigationItemSelectedListener { item ->
-            when (item.itemId) {
-                R.id.home -> {
-                    replaceFragment(HomeFragment())
-                    true
-                }
+        val navView:BottomNavigationView=_binding.navigationViewBtm
 
-                R.id.portfolio -> {
-                    replaceFragment(PortfolioListFragment())
-                    true
-                }
+        val navController= findNavController(R.id.fragment_container_view)
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,R.id.navigation_assets, R.id.navigation_portfolio,R.id.navigation_settings
+            )
+        )
+        setupActionBarWithNavController(navController,appBarConfiguration)
 
-                R.id.assets -> {
-                    replaceFragment(AssetListFragment())
-                    true
-                }
+        navView.setupWithNavController(navController)
 
-                R.id.settings -> {
-                    replaceFragment(SettingsFragment())
-                    true
-                }
-
-                else -> false
-            }
-        }
+    //        _binding.navigationViewBtm.setOnNavigationItemSelectedListener { item ->
+    //            when (item.itemId) {
+    //                R.id.navigation_home -> {
+    //                    replaceFragment(HomeFragment())
+    //                    true
+    //                }
+    //
+    //                R.id.navigation_portfolio -> {
+    //                    replaceFragment(PortfolioListFragment())
+    //                    true
+    //                }
+    //
+    //                R.id.navigation_assets -> {
+    //                    replaceFragment(AssetListFragment())
+    //                    true
+    //                }
+    //
+    //                R.id.navigation_settings -> {
+    //                    replaceFragment(SettingsFragment())
+    //                    true
+    //                }
+    //
+    //                else -> false
+    //            }
+    //        }
     }
 
     private fun replaceFragment(fragment: Fragment) {
