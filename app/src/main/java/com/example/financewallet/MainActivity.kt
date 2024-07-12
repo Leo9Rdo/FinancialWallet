04dce3c6f112a6bc03593a38dc08aafbb3f0157f
John <muravei20004@mail.ru>
2024-07-12 20:00:15 +0300

    delete replaceFragment and FragmentTransaction

--- app/src/main/java/com/example/financewallet/MainActivity.kt
+++ app/src/main/java/com/example/financewallet/MainActivity.kt
@@ -2,13 +2,11 @@ package com.example.financewallet
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.financewallet.databinding.ActivityMainBinding
-import com.example.financewallet.presentation.home.HomeFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -19,7 +17,6 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(_binding.root)
-        replaceFragment(HomeFragment())
 
         val navView: BottomNavigationView = _binding.navigationViewBtm
 
@@ -35,12 +32,5 @@ class MainActivity : AppCompatActivity() {
         setupActionBarWithNavController(navController, appBarConfiguration)
 
         navView.setupWithNavController(navController)
-    }
-
-    private fun replaceFragment(fragment: Fragment) {
-        val fragmentManager = supportFragmentManager
-        val fragmentTransaction = fragmentManager.beginTransaction()
-        fragmentTransaction.replace(R.id.fragment_container_view, fragment)
-        fragmentTransaction.commit()
     }
 }
