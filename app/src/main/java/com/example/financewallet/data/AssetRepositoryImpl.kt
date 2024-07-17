749e4a9f247024b16d7d1b23d842a9667fd95c68
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2024-07-17 13:26:11 +0300

    update asset repo impl and rewrite to sealed class

--- app/src/main/java/com/example/financewallet/data/AssetRepositoryImpl.kt
+++ app/src/main/java/com/example/financewallet/data/AssetRepositoryImpl.kt
@@ -4,7 +4,7 @@ import com.example.financewallet.domain.entity.Asset
 import com.example.financewallet.domain.repository.AssetRepository
 
 class AssetRepositoryImpl : AssetRepository {
-    private val assets = StubAssetList.assets.toMutableList()
+    private val assets = StubAssetList.assets
 
     override fun getAllAssets(): List<Asset> = assets.toList()
 }
