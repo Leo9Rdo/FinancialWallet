7bf466581b750f6677f231a41f9524ec2f306875
Andrew Ryabitskiy <andrew.ryabitskiy@gmail.com>
2024-08-04 04:11:26 +0300

    update assets with coroutines to work with api fetch currency

--- app/src/main/java/com/example/financewallet/data/AssetRepositoryImpl.kt
+++ app/src/main/java/com/example/financewallet/data/AssetRepositoryImpl.kt
@@ -1,11 +1,17 @@
 package com.example.financewallet.data
 
 import com.example.financewallet.domain.entity.Asset
+import com.example.financewallet.domain.interactor.CurrencyInteractor
 import com.example.financewallet.domain.repository.AssetRepository
 import javax.inject.Inject
 
-class AssetRepositoryImpl @Inject constructor() : AssetRepository {
+class AssetRepositoryImpl @Inject constructor(
+    private val currencyInteractor: CurrencyInteractor
+) : AssetRepository {
     private val assets = StubAssetList.assets
 
-    override fun getAllAssets(): List<Asset> = assets.toList()
+    override suspend fun getAllAssets(): List<Asset> {
+        StubAssetList.stubAssets(currencyInteractor)
+        return assets.toList()
+    }
 }
